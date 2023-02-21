edeac2ae21501911cb747093c73bb34fd9f0faca
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-02-21 14:50:21 +0530

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,43 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Akshat Sharma",  "Web developer", "Problem solver","Graphic Designer"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.add("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) {
+      textArrayIndex = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (textArray.length) {
+    setTimeout(type, newTextDelay + 250);
+  }
+});
---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,12 @@
+$('.about-info-inner').typed({
+  strings: [
+    "<span>Namaste Myself Akshat Sharma<br><br>The skill I have is Software development. I am also an open-source contributor. I have worked with HTML, CSS, and JavaScript. I know C, C++, and Java programming languages. In addition, I also know Bash scripting language and have worked in the cybersecurity field as well. I am also familiar with operating systems and networking. I have worked in Kali Linux, Ubuntu, Garuda Linux, and Windows as well. I am also a competitive coder and have solved 1100+ DSA questions in LeetCode, Geek for Geeks, CodeChef, and Codeforces. I also know Database management like RDBMS like SQL, MYSQL, and Oracle.</span>"
+  ],
+  typeSpeed: 30,
+  backSpeed: 10,
+  startDelay: 1000,
+  cursorChar: '|',
+  loop: true,
+  contentType: 'html'
+});
+
